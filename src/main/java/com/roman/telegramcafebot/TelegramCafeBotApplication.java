7a630683d572e2588d3eb9125e5d2fb19fbd4ac9
Roman Kamentsa <mrrubox1@gmail.com>
2023-11-22 09:00:55 +0100

    begin

---
+++ src/main/java/com/roman/telegramcafebot/TelegramCafeBotApplication.java
@@ -0,0 +1,13 @@
+package com.roman.telegramcafebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramCafeBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramCafeBotApplication.class, args);
+	}
+
+}
